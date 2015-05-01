a5eef6a40bc77b57fc17be459826b8ef72f3236c
Logan Mitchell <mitchlam711@gmail.com>
2015-05-01 11:24:21 -0600

    Added V-measure

--- driver.py
+++ driver.py
@@ -8,6 +8,7 @@ import INCREMENT
 
 from sklearn.cluster import KMeans
 from sklearn import datasets
+from sklearn import metrics
 from scipy.spatial import distance as Distance
 
 
@@ -22,74 +23,149 @@ class Instance:
         return Distance.euclidean(x.data,y.data)
 
 
-def confusion(clustering):
+def contingency(clustering):
     labels = [-1]*len(clustering)
 
-    conf = {}
+    cont = {}
 
     for i,cluster in enumerate(clustering):
         trueVals = map(lambda x: x.label, cluster)
         label = utils.mode(trueVals)
         
         for l in trueVals:
-            if l not in conf:
-                conf[l] = [0]*len(labels)
+            if l not in cont:
+                cont[l] = [0]*len(labels)
             
-            conf[l][i] += 1
+            cont[l][i] += 1
 
         
         
         
 
     final = []
-    for key,value in conf.items():
+    for key,value in cont.items():
         final.append(value)
 
     #print "Final Error : %d of %d" % (error, total)
     return final
 
-def checkAccuracy(clustering):
-    labels = [-1]*len(clustering)
 
-    conf = {}
-    error = 0.0
-    total = 0
-    for cluster in clustering:
-        trueVals = map(lambda x: x.label, cluster)
-        label = utils.mode(trueVals)
-        
-        #print "Label: %d" % (label)
-        
-        e = 0.0
-        for l in trueVals:
-            if l not in conf:
-                conf[l] = [0]*len(labels)
-            
-            conf[l][label] += 1
-            if l != label:
-                e += 1
-        
-        #print "Error: %d of %d: %f" % (e, len(cluster), e/len(cluster)) 
-        error += e
-        total += len(cluster)
-        
-    if total == 0:
-        return (0,0,0)
+def homogeneity(cont,eps=10e-10):
+    if(len(cont[0]) == 1):
+        return 1.0
     
+    x = 0.0
+    label_counts = map(sum,cont)
+    N = float(sum(label_counts))
+    cluster_sizes = [0.0]*len(cont[0])
+    num_labels = len(cont)
+    
+    #H(C|K)
+    for label, row in enumerate(cont):
+        for cluster, val in enumerate(row):
+                cluster_sizes[cluster] += val
+    
+    for label, row in enumerate(cont):
+        for cluster, val in enumerate(row):
+            one = val/float(N)
+            two = val/ cluster_sizes[cluster]
+            if (two + eps != 0.0):
+                x += one * np.log(two+eps)
+    
+    x *= -1
+    
+    y = 0.0
+    
+    #H(C)
+    for count in label_counts:
+        num = eps + count / N
+        if(num != 0.0):
+            y += (num) * np.log( num ) #different from paper
+    
+    y *= -1
+    
+    return 1.0 - (x / y)
+
+def completeness(cont, eps=10e-10):
+    if(len(cont[0]) == 1):
+        return 1.0
+    
+    x = 0.0
+    label_counts = map(sum,cont)
+    N = sum(label_counts)
+    cluster_sizes = [0.0]*len(cont[0])
+    num_labels = len(cont)
+    
+    
+    for label, row in enumerate(cont):
+        for cluster, val in enumerate(row):
+            #H(K|C)
+            one = val/float(N)
+            two = val/float(label_counts[label])
+            if (two+eps != 0.0):
+                x += one * np.log(two+eps)
+            
+            #H(K)
+            cluster_sizes[cluster] += val
+    
+    x *= -1
+    
+    y = 0.0
+    
+    for size in cluster_sizes:
+        num = eps + size / N
+        if(num != 0.0):
+            y += (num) * np.log( num ) #Different from paper
+    
+    y *= -1
+    
+    return 1.0 - (x / y)
+
+def V_measure(cont, B=1):
+    h = homogeneity(cont)
+    c = completeness(cont)
+    
+    v = ((1 + B) * h * c) / (( B * h) + c)
+    
+    return v
+
+def All_measures(cont, B=1):
+    h = homogeneity(cont)
+    c = completeness(cont)
+    
+    v = ((1 + B) * h * c) / (( B * h) + c)
+    
+    return (h,c,v)
+
+def checkAccuracy(cont):
+
+    total = 0.0
+    correct =  0.0
+    
+    m = [0] * len(cont[0])
+    idx = [0] * len(cont[0])
+    
+    for i,r in enumerate(cont):
+        for j,v in enumerate(r):
+            if i == j:
+                total += v
+            else:
+                total += v
+            
+            if(v > m[j]):
+                idx[j] = i
+                m[j] = v
+    
+    for i,r in enumerate(cont):
+        for j,v in enumerate(r):
+            if i == idx[j]:
+                correct += v
+
+    error = total-correct
     #print "Final Error : %d of %d" % (error, total)
     return (error, total, error/total)
 
-def main(args):
-
-    iris = datasets.load_iris()
-
-    X = iris.data
-    Y = iris.target
-
-    print "Iris: %d" %(len(X))
-
-    K = 3
-    
+def cluster_kmeans(X, Y, K=3):
     clusters = []
     
     for i in range(K):
@@ -99,16 +175,32 @@ def main(args):
 
     kmeans.fit(X)
     labels = kmeans.labels_
-    
+
+    measures = metrics.homogeneity_completeness_v_measure(Y, labels)    
 
     for x,y,t in zip(X,labels,Y):
         #print "Predicted: %d Actual: %d: Instance: %s" %(y,t,str(x))
         clusters[y].append(Instance(x,t))
+        
+    print "KMeans: H: %f, C: %f, V: %f" %(measures)
+    return clusters
 
-    print "Initial Error: %d of %d: %f" % (checkAccuracy(clusters))
-    conf = confusion(clusters)
+def main(args):
+
+    iris = datasets.load_iris()
+
+    X = iris.data
+    Y = iris.target
+
+    print "Iris: %d" %(len(X))
     
-    for i,line in enumerate(conf):
+    clusters = cluster_kmeans(X,Y,K=3)
+
+    cont = contingency(clusters)
+    print "Initial Error: %d of %d: %f" % (checkAccuracy(cont))
+    print "H: %f C: %f V: %f" % (All_measures(cont))
+
+    for i,line in enumerate(cont):
         print "%d:\t%s" % (i, str(line))
     
     increment = INCREMENT.OpticsINCREMENT(clusters, Instance.distance)
@@ -118,11 +210,11 @@ def main(args):
     increment.queryUser()
     increment.mergeSubclusters()
     
+    cont = contingency(increment.final)
+    print "Final Error: %d of %d: %f" % (checkAccuracy(cont))
+    print "H: %f C: %f V: %f" % (All_measures(cont))
     
-    print "Final Error: %d of %d: %f" % (checkAccuracy(increment.final))
-    conf = confusion(increment.final)
-    
-    for i,line in enumerate(conf):
+    for i,line in enumerate(cont):
         print "%d:\t%s" % (i, str(line))
 
 if __name__ == "__main__":
