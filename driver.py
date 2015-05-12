256002cf0e9b5c02bcfec7d0e7cc8ded810d381c
Logan Mitchell <mitchlam711@gmail.com>
2015-05-12 11:52:55 -0600

    Added ability to form clusters from generalization output of supervised classification algorithms.

--- driver.py
+++ driver.py
@@ -12,9 +12,15 @@ import INCREMENT
 from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import SpectralClustering
+
+from sklearn.linear_model import Perceptron
+from sklearn.svm import SVC
+
 from sklearn import datasets
 from sklearn import metrics
 from scipy.spatial import distance as Distance
+
+from sklearn.cross_validation import train_test_split
 
 
 class Instance:
@@ -28,16 +34,50 @@ class Instance:
         return Distance.euclidean(x.data,y.data)
 
 
-def cluster_data(X,Y, method="kmeans", K=5, **kwargs):
+def classify_data(X,Y, args, holdout = 0.8):
+    X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=holdout, random_state=np.random.RandomState())
+    
+    model = None
+    if (args.initial == "perceptron"):
+        model = Perceptron()
+    elif(args.initial == "svm"):
+        model = SVC(kernel="poly")
+    else:
+        raise("Model Not Supported.")
+    
+    model.fit(X_train, Y_train)
+    
+    Y_pred = model.predict(X_test)
+    
+    labels = set(Y_pred)
+    
+    k = len(labels)
+    
+    labels = list(labels)
+    
+    clusters = []
+    
+    for i in range(k):
+        clusters.append([])
+    
+    for x,y,t in zip(X_test, Y_pred, Y_test):
+        clusters[labels.index(y)].append(Instance(x,t))
+        
+    return clusters
+    
+
+def cluster_data(X,Y, args):
     
     alg = None
     
-    if (method == "dbscan"):
-        alg = DBSCAN(**kwargs)
-    elif (method == "spectral"):
-        alg = SpectralClustering(n_clusters=K, **kwargs)
+    if (args.initial == "dbscan"):
+        alg = DBSCAN()
+    elif (args.initial == "spectral"):
+        alg = SpectralClustering(n_clusters=args.K, affinity="nearest_neighbors")
+    elif (args.initial == "kmeans"):
+        alg = KMeans(n_clusters=args.K)
     else:
-        alg = KMeans(n_clusters=K, **kwargs)
+        raise("Model Not Supported.")
         
     alg.fit(X)
     
@@ -106,16 +146,17 @@ def main(args):
     
     print "Using: %s (%d)" % (args.dataset, len(X))
     
-    #clusters = cluster_data(X,Y, method = "kmeans", K=args.K)
-    #clusters = cluster_data(X,Y, method = "dbscan", eps=0.5, min_samples=5)
-    clusters = cluster_data(X,Y, method = "spectral", K=args.K, affinity="nearest_neighbors")
-    #clusters = cluster_kmeans(X,Y,K=1)
-    #clusters = cluster_dbscan(X,Y, e=0.5, minPts=5 )
+    clusters = []
     
-    instances = []
+    print "Initial Clustering:", args.initial
     
-    for x,y in zip(X,Y):
-        instances.append(Instance(x, y))
+
+        
+    if (args.supervised):
+        clusters = classify_data(X,Y, args)
+    else:
+        clusters = cluster_data(X,Y, args)
+        
 
     print "Initial:"
     validation.printMetrics(clusters)
@@ -157,6 +198,8 @@ if __name__ == "__main__":
     parser.add_argument( "-q", "--query-size", help="The number of points to present to the user per query.", type=int, default=9)
     parser.add_argument( "-t", "--times-presented", help="The minimum number of times a point is presented to the user.", type=int, default=1)
     parser.add_argument( "-k", metavar="Clusters" , help="The number of clusters to use with the initial clustering algorithm (where applicable).", type=int, default=20, dest="K")
+    parser.add_argument("-i", "--initial", help="Initial clustering algorithm", type=str, default = "kmeans")
+    parser.add_argument("-s", "--supervised", help="Specify whether or not to use a supervised model to form initial clustering.", action="store_true")
     
     
     args = parser.parse_args()
